dff8d7a9ae791216edcebf4a7b97d049f02864aa
Yasiukovich Maksim <kazanova_69_res@mail.ru>
2021-08-31 21:39:27 +0300

    Моя машина на AV!

--- tasks/variables/car_price.py
+++ tasks/variables/car_price.py
@@ -29,7 +29,7 @@ def car_price(price: float) -> float:
 
     :return: Стоимость автомобиля с наценками
     """
-    result = None
+    result = price + (price / 100 * TAX) + (price / 100 * REGISTRATION_FEE) + AGENCY_FEE + DELIVERY_PRICE
     return result
 
 
