a0c73cae617612c7c617c4385f22715091d22b98
Maksim Kisel <mkisel28@gmail.com>
2021-11-20 03:57:46 +0300

    comlate

--- tasks/variables/triangle.py
+++ tasks/variables/triangle.py
@@ -1,33 +1,11 @@
-"""
-ЗАДАНИЕ
---------------------------------------------------------------------------------
-Функция triangle получает длины 2х катетов прямоугольного треугольника.
-Нужно отредактировать функцию таким образом,
-чтобы она возвращала периметр, площадь и гипотенузу
-
-ПРИМЕРЫ
---------------------------------------------------------------------------------
-triangle(3, 4) -> (5, 12, 6)
-"""
+import math
 
 
 def triangle(side_1: int, side_2: int) -> tuple:
-    """
-    Рассчитывает гипотенузу, периметр и площадь
-
-    :param side_1: первый катет
-    :type side_1: int
-
-    :param side_2: второй катет
-    :type side_2: int
-
-    :return: кортеж с параметрами
-    :rtype: tuple
-    """
-    hypotenuse = None
-    perimeter = None
-    square = None
-    return hypotenuse, perimeter, square
+    hypotenuse = (side_1 ** 2 + side_2 ** 2) ** .5
+    perimeter = (side_1 + side_2 + math.sqrt(side_1 * side_1 + side_2 * side_2))
+    square = (side_1 * side_2 / 2)
+    return int(hypotenuse), int(perimeter), int(square)
 
 
 if __name__ == '__main__':
